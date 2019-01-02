50ef5521a62a6b754fe494c43fb18c523a154faa
karthik540 <36918151+karthik540@users.noreply.github.com>
2019-01-02 20:16:12 +0530

    Initial commit

---
+++ guiInterface.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from main import *
+
+def loadMyMessage(name , Message):
+    if Message != '':
+        chatWindow.config(state = NORMAL)
+        lineNumber = float(chatWindow.index('end'))-1.0
+        chatWindow.insert(END , name + ": " + Message)
+        chatWindow.tag_add(name, lineNumber, lineNumber+0.4)
+        chatWindow.tag_config(name, foreground="#FF8000", font=("Arial", 12, "bold"))
+        chatWindow.config(state=DISABLED)
+        chatWindow.yview(END)
+
+
+def ClickAction():
+    # Filtering the Message
+    filteredMessage = messageFilter(EntryBox.get("0.0" , END))
+    print(filteredMessage)
+
+    # Loading the User Message
+    loadMyMessage("You" , filteredMessage)
+    EntryBox.delete("0.0" , END)        # Clears the entry message box
+    chatWindow.yview(END)               # Moves the Chat window down 
+    
+    # Loading the Bot Message
+    botMessage = botResponseReciever(filteredMessage[:-1])
+    loadMyMessage("Deku" , botMessage)
+    chatWindow.yview(END)  
+
+###       GUI     ###
+
+
+# Window Management
+root = Tk()
+root.title("Deku Bot")
+root.geometry("400x500")
+root.resizable(height= False , width= False)
+
+# Chat Window
+chatWindow = Text(root , bd = 0 , bg = "white" ,height = "8" , width = "50" , font = "Ariel")
+#chatWindow.insert(END , "Start your conversation !")
+chatWindow.config(state = DISABLED)
+
+# ScrollBar
+scrollBar = Scrollbar(root , command = chatWindow.yview)
+chatWindow['yscrollcommand'] = scrollBar.set
+
+#Send Button
+SendButton = Button(root, font=30, text="Send", width="12", height="5",
+                    bd=0, bg="#FFBF00", activebackground="#FACC2E",
+                    command = ClickAction)
+
+EntryBox = Text(root, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox.bind("<Return>", """DisableEntry""")
+EntryBox.bind("<KeyRelease-Return>", """PressAction""")
+
+# Placing all the stuff inplace
+scrollBar.place(x=376,y=6, height=386)
+chatWindow.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+import json
+import apiai
+from tkinter import *
+
+CLIENT_ACCESS_TOKEN = '3a79f257e43e497b9d07d18c51e7497d'
+
+def messageFilter(EntryText):
+    endText = ''
+    for i in range(len(EntryText)-1 , -1 , -1):
+        if EntryText[i] != '\n':
+            endText = EntryText[0 : i+1]
+            break
+    for i in range(0 , len(endText) , 1):
+        if EntryText[i] != '\n':
+            return endText[i:] + "\n"
+    return ''
+
+def botResponseReciever(queryMessage):
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
+    request = ai.text_request()
+
+    request.query = queryMessage
+
+    response = request.getresponse()
+
+    rawData = str(response.read())
+    rawData = rawData.replace(r"\n" , "")       #Remove \n 
+    rawData = rawData.replace(r"b'" , "" , 1)   #Remove b'
+    rawData = rawData.replace(r"\'" , "" , 1)   #Remove \' which causes prob in the bot message
+    jsonData = rawData[0:-1]                        #Remove ' in the end
+
+    jsonObj = open("response.json" , "w+")
+    jsonObj.write(jsonData)
+    jsonObj.close()
+
+    with open('response.json') as f:
+        data = json.load(f)
+    return data['result']['fulfillment']['speech'] + "\n"
